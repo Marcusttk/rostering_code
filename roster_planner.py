29495dd7ed430d844620c34b7072f48cc5c84ad2
Work laptop <marcusttk@gmail.com>
2025-08-12 18:16:34 +0800

    commit from Marcus' laptop

--- roster_planner.py
+++ roster_planner.py
@@ -1,8 +1,7 @@
 from ics import Calendar
 import json
 
-# update all shorthands in the fg1_names.json also
-people_dict = json.load(open("./fg1_names.json"))
+
 # Open the .ics file
 with open("C:/Users/User/Downloads/FG1 OOO Calendar (1).ics", "r", encoding="utf-8") as f:
     calendar = Calendar(f.read())
@@ -17,7 +16,7 @@ def read_calendar(calendar_object):
         print("-" * 40)
 
 
-def build_calendar_dict(calendar_object):
+def build_calendar_dict(calendar_object, people_dict):
     for event in calendar.events:
 
 def update_calendar(calendar_dict):
@@ -25,6 +24,8 @@ def update_calendar(calendar_dict):
 
 
 def main():
+    # update all shorthands in the fg1_names.json also
+    people_dict = json.load(open("./fg1_names.json"))
     print(1)
 
 
