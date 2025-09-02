7f3165fa992f8d1a6d060a37c7fa22f7d5278d2c
Work laptop <marcusttk@gmail.com>
2025-09-02 14:32:36 +0800

    commit from Marcus' laptop

--- roster_planner.py
+++ roster_planner.py
@@ -1,11 +1,11 @@
-from ics import Calendar  #pip3 install ics
+from ics import Calendar  # pip3 install ics
 import json
 from datetime import date, datetime, timedelta
 
 # Open the .ics file, adjust this accordingly
 desktop_file_path = "C:/Users/ace-j/Downloads/" + "FG1 OOO Calendar.ics"
 laptop_file_path = "C:/Users/User/Downloads/" + "FG1 OOO Calendar.ics"
-with open(desktop_file_path, "r", encoding="utf-8") as f:
+with open(laptop_file_path, "r", encoding="utf-8") as f:
     calendar = Calendar(f.read())
 
 
@@ -26,34 +26,43 @@ def read_calendar(calendar_object):
 def check_date(current_date, datetime_str):
     # format of datetime_str = 2025-09-11T00:00:00+00:00
     # Convert string to datetime object
-    datetime_object = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S+00:00")
-    if datetime_object < current_date:
+    datetime_str = str(datetime_str).split("T")[0]
+    datetime_object = datetime.strptime(str(datetime_str), "%Y-%m-%d")
+    # Convert datetime to date
+    date_only = datetime_object.date()
+    if date_only < current_date:
         return False
     else:
         return True
 
 
 def return_dates(start_date, end_date):
-    start = datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%S+00:00")
-    end = datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S+00:00")
-    dates = [start + timedelta(days=i) for i in range((end - start).days + 1)]
+    start_str = str(start_date).split("T")[0]
+    start = datetime.strptime(start_str, "%Y-%m-%d")
+    end_str = str(end_date).split("T")[0]
+    end = datetime.strptime(end_str, "%Y-%m-%d")
+    dates = [start + timedelta(days=i) for i in range((end - start).days)]
     dates_str = []
     for date_entry in dates:
         date_value_str = date_entry.strftime("%Y-%m-%d")
         date_list = date_value_str.split("-")
-        dates_str.append([date_list[0], date_list[1], date_list[2]])
+        dates_str.append([int(date_list[0]), int(date_list[1]), int(date_list[2])])
     return dates_str
 
 
 def create_year_month_entry(ooo_dict, person):
-    year = date.today().year
+    year = int(date.today().year)
     if person not in ooo_dict:
         # creates a year with months 1 to 12
-        ooo_dict[person] = {year: {i: {} for i in range(1, 13)}}
+        ooo_dict[person] = {year: {i: {} for i in range(1, 13)},
+                            year + 1: {i: {} for i in range(1, 13)}}
     else:
         # this creates a new year entry for that person
         if year not in ooo_dict[person]:
             ooo_dict[person][year] = {i: {} for i in range(1, 13)}
+        if year + 1 not in ooo_dict[person]:
+            ooo_dict[person][year + 1] = {i: {} for i in range(1, 13)}
+    # print(ooo_dict)
     return ooo_dict
 
 
@@ -63,7 +72,7 @@ def update_ooo_dict(dates, person, ooo_dict):
     ooo_dict = create_year_month_entry(ooo_dict, person)
     # format of entries = [year, month, day]
     for entries in dates:
-        ooo_dict[person][entries[0]][entries[1]] = entries[2]
+        ooo_dict[person][entries[0]][entries[1]][entries[2]] = 0
     return ooo_dict
 
 
@@ -72,13 +81,16 @@ def build_calendar_dict(people_dict, ooo_dict):
     for event in calendar.events:
         relevant_event = check_date(now, event.begin)
         if relevant_event:
+            name_found = False
             for names in people_dict:
-                if names in event.name:
+                if names in str(event.name).lower():
                     dates = return_dates(event.begin, event.end)
                     # calculate dates to put in ooo_dict
-                    ooo_dict = update_ooo_dict(dates, names, ooo_dict)
-                else:
-                    print("no person indicated in: " + event.name)
+                    ooo_dict = update_ooo_dict(dates, people_dict[names], ooo_dict)
+                    name_found = True
+                    break
+            if not name_found:
+                print("no person indicated in: " + event.name)
     return ooo_dict
 
 
@@ -90,13 +102,12 @@ def main():
     # update all shorthands in the fg1_names.json also
     people_dict = json.load(open("./fg1_names.json"))
     ooo_dict = json.load(open("./people_ooo.json"))
-    ooo_dict = build_calendar_dict(people_dict, ooo_dict)
+    ooo_dict = build_calendar_dict(people_dict["all"], ooo_dict)
 
-    with open("./people_ooo.json", "w") as f:
-        json.dump(ooo_dict, f, indent=4)
+    with open("./people_ooo.json", "w") as file:
+        json.dump(ooo_dict, file, indent=4)
     # ics file is deleted so that you don't have to keep changing the name to the latest copy.
 
 
 if __name__ is main():
     main()
-
